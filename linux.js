0b32d0d817ddab24a3069dae33396af35018ef85
Kevin Regner <krw10108@saintjoe.edu>
2015-03-18 23:02:41 -0500

    Dates added

--- linux.js
+++ linux.js
@@ -1,12 +1,12 @@
 var LINUX = [
-  {name: "Astra", DevelopedBy: "RusBitTech", country: "Russia", Bit: "64", Date: ""},
-  {name: "Corel", DevelopedBy: "Xandros", country: "Canada", Bit: "32", Date: ""},
-  {name: "Dream", DevelopedBy: "", country: "Canada", Bit: "64", Date: ""},
-  {name: "Handy", DevelopedBy: "Arpinux", country: "France", Bit: "64", Date: ""},
-  {name: "Symphony", DevelopedBy: "Ryan P Quinn", country: "Unknown", Bit: "64", Date: ""},
-  {name: "Skole", DevelopedBy: "Skolelinux team", country: "Norway", Bit: "64", Date: ""},
-  {name: "Maemo", DevelopedBy: "Hildon Foundation", country: "Unknown", Bit: "64", Date: ""},
-  {name: "Kali", DevelopedBy: "Offensive Security", country: "Unknown", Bit: "64", Date: ""}
+  {name: "Astra", DevelopedBy: "RusBitTech", country: "Russia", Bit: "64", Date: "10/17/2010"},
+  {name: "Corel", DevelopedBy: "Xandros", country: "Canada", Bit: "32", Date: "11/15/1999"},
+  {name: "Dream", DevelopedBy: "", country: "Canada", Bit: "64", Date: "10/21/2010"},
+  {name: "Handy", DevelopedBy: "Arpinux", country: "France", Bit: "64", Date: "09/15/2013"},
+  {name: "Symphony", DevelopedBy: "Ryan P Quinn", country: "Unknown", Bit: "64", Date: "06/29/2011"},
+  {name: "Skole", DevelopedBy: "Skolelinux team", country: "Norway", Bit: "64", Date: "06/20/2004"},
+  {name: "Maemo", DevelopedBy: "Hildon Foundation", country: "Unknown", Bit: "64", Date: "11/01/2011"},
+  {name: "Kali", DevelopedBy: "Offensive Security", country: "Unknown", Bit: "64", Date: "02/09/2015"}
 ];
 
 if (typeof module != "undefined" && module.exports)
