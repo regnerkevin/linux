79b7daa2ce13c84b9efd6c2c5684bf86ef6ea405
Kevin Regner <krw10108@saintjoe.edu>
2015-03-18 23:05:42 -0500

    Update linux.js

--- linux.js
+++ linux.js
@@ -1,7 +1,7 @@
 var LINUX = [
   {name: "Astra", DevelopedBy: "RusBitTech", country: "Russia", Bit: "64", Date: "10/17/2010"},
   {name: "Corel", DevelopedBy: "Xandros", country: "Canada", Bit: "32", Date: "11/15/1999"},
-  {name: "Dream", DevelopedBy: "", country: "Canada", Bit: "64", Date: "10/21/2010"},
+  {name: "Dream", DevelopedBy: "Unix-like", country: "Canada", Bit: "64", Date: "10/21/2010"},
   {name: "Handy", DevelopedBy: "Arpinux", country: "France", Bit: "64", Date: "09/15/2013"},
   {name: "Symphony", DevelopedBy: "Ryan P Quinn", country: "Unknown", Bit: "64", Date: "06/29/2011"},
   {name: "Skole", DevelopedBy: "Skolelinux team", country: "Norway", Bit: "64", Date: "06/20/2004"},
