12311c9d7f2e77d2971674e5a773b250cfc868e5
Kevin Regner <krw10108@saintjoe.edu>
2015-03-19 03:12:44 -0500

    Update filter.js

--- filter.js
+++ filter.js
@@ -11,6 +11,6 @@ function filter(array, test) {
 
 }
 
-print(JSON.stringify(filter(linux, function(getdate) {
+consolelog(JSON.stringify(filter(linux, function(getdate) {
 	return getdate.DatebyMS > 1000000000000;
 })));
