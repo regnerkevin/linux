aebb84a77ac49ad2982914a6378b852825e7e53f
Kevin Regner <krw10108@saintjoe.edu>
2015-03-19 03:10:10 -0500

    Update linux.js

--- linux.js
+++ linux.js
@@ -5,7 +5,8 @@ var LINUX = "[\n  " + [
   '{"name": "Handy", "DevelopedBy": "Arpinux", "country": "France", "Bit": "64", "Date": "09/15/2013", "DatebyMS": "1379221200000"}',
   '{"name": "Symphony", "DevelopedBy": "Ryan P Quinn", "country": "Unknown", "Bit": "64", "Date": "06/29/2011", "DatebyMS": "1309323600000"}',
   '{"name": "Skole", "DevelopedBy": "Skolelinux team", "country": "Norway", "Bit": "64", "Date": "06/20/2004", "DatebyMS": "1087707600000"}',
-  '{"name": "Maemo", "DevelopedBy": "Hildon Foundation", "country": "Unknown", "Bit": "64", "Date": "11/01/2011", "DatebyMS": "1320123600000"}',  '{"name": "Kali", "DevelopedBy": "Offensive Security", "country": "Unknown", "Bit": "64", "Date": "02/09/2015", "DatebyMS": "1423461600000"}
+  '{"name": "Maemo", "DevelopedBy": "Hildon Foundation", "country": "Unknown", "Bit": "64", "Date": "11/01/2011", "DatebyMS": "1320123600000"}', 
+  '{"name": "Kali", "DevelopedBy": "Offensive Security", "country": "Unknown", "Bit": "64", "Date": "02/09/2015", "DatebyMS": "1423461600000"}
 ].join(",\n  ") + "\n]";
 
 if (typeof module != "undefined" && module.exports)
