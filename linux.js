d4b5f88b1645f16e28949c8d56d4408ba9772b57
Kevin Regner <krw10108@saintjoe.edu>
2015-03-18 22:51:47 -0500

    Updated list, need dates :/

--- linux.js
+++ linux.js
@@ -1,1 +1,13 @@
-jdafhbgkjasdhgajhb
+var LINUX = [
+  {name: "Astra", DevelopedBy: "RusBitTech", country: "Russia"},
+  {name: "Corel", DevelopedBy: "Xandros", country: "Canada"},
+  {name: "Dream", DevelopedBy: "", country: "Canada"},
+  {name: "Handy", DevelopedBy: "Arpinux", country: "France"},
+  {name: "Symphony", DevelopedBy: "Ryan P Quinn", country: "Unknown"},
+  {name: "Skole", DevelopedBy: "Skolelinux team", country: "Norway"},
+  {name: "Maemo", DevelopedBy: "Hildon Foundation", country: "Unknown"},
+  {name: "Kali", DevelopedBy: "Offensive Security", country: "Unknown"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = LINUX;
