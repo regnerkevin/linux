9a85d91a43d6a255fe9cdc3675e8e4fbdf06e480
Kevin Regner <krw10108@saintjoe.edu>
2015-03-18 22:57:02 -0500

    Updated a numerical value

--- linux.js
+++ linux.js
@@ -1,12 +1,12 @@
 var LINUX = [
-  {name: "Astra", DevelopedBy: "RusBitTech", country: "Russia"},
-  {name: "Corel", DevelopedBy: "Xandros", country: "Canada"},
-  {name: "Dream", DevelopedBy: "", country: "Canada"},
-  {name: "Handy", DevelopedBy: "Arpinux", country: "France"},
-  {name: "Symphony", DevelopedBy: "Ryan P Quinn", country: "Unknown"},
-  {name: "Skole", DevelopedBy: "Skolelinux team", country: "Norway"},
-  {name: "Maemo", DevelopedBy: "Hildon Foundation", country: "Unknown"},
-  {name: "Kali", DevelopedBy: "Offensive Security", country: "Unknown"}
+  {name: "Astra", DevelopedBy: "RusBitTech", country: "Russia", Bit: "64", Date: ""},
+  {name: "Corel", DevelopedBy: "Xandros", country: "Canada", Bit: "32", Date: ""},
+  {name: "Dream", DevelopedBy: "", country: "Canada", Bit: "64", Date: ""},
+  {name: "Handy", DevelopedBy: "Arpinux", country: "France", Bit: "64", Date: ""},
+  {name: "Symphony", DevelopedBy: "Ryan P Quinn", country: "Unknown", Bit: "64", Date: ""},
+  {name: "Skole", DevelopedBy: "Skolelinux team", country: "Norway", Bit: "64", Date: ""},
+  {name: "Maemo", DevelopedBy: "Hildon Foundation", country: "Unknown", Bit: "64", Date: ""},
+  {name: "Kali", DevelopedBy: "Offensive Security", country: "Unknown", Bit: "64", Date: ""}
 ];
 
 if (typeof module != "undefined" && module.exports)
