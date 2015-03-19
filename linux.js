77af8cf09fe21958c9956788433b61bf3c4c8e13
Kevin Regner <krw10108@saintjoe.edu>
2015-03-19 03:04:17 -0500

    Update linux.js

--- linux.js
+++ linux.js
@@ -1,13 +1,12 @@
-var LINUX = [
-  {"name": "Astra", "DevelopedBy": "RusBitTech", "country": "Russia", "Bit": "64", "Date": "10/17/2010", "DatebyMS": "1287291600000"},
-  {"name": "Corel", "DevelopedBy": "Xandros", "country": "Canada", "Bit": "32", "Date": "11/15/1999", "DatebyMS": "942645600000"},
-  {"name": "Dream", "DevelopedBy": "Unix-like", "country": "Canada", "Bit": "64", "Date": "10/21/2010", "DatebyMS": "1287637200000"},
-  {"name": "Handy", "DevelopedBy": "Arpinux", "country": "France", "Bit": "64", "Date": "09/15/2013", "DatebyMS": "1379221200000"},
-  {"name": "Symphony", "DevelopedBy": "Ryan P Quinn", "country": "Unknown", "Bit": "64", "Date": "06/29/2011", "DatebyMS": "1309323600000"},
-  {"name": "Skole", "DevelopedBy": "Skolelinux team", "country": "Norway", "Bit": "64", "Date": "06/20/2004", "DatebyMS": "1087707600000"},
-  {"name": "Maemo", "DevelopedBy": "Hildon Foundation", "country": "Unknown", "Bit": "64", "Date": "11/01/2011", "DatebyMS": "1320123600000"},
-  {"name": "Kali", "DevelopedBy": "Offensive Security", "country": "Unknown", "Bit": "64", "Date": "02/09/2015", "DatebyMS": "1423461600000"}
-];
+var LINUX = "[\n  " + [
+  '{"name": "Astra", "DevelopedBy": "RusBitTech", "country": "Russia", "Bit": "64", "Date": "10/17/2010", "DatebyMS": "1287291600000"}',
+  '{"name": "Corel", "DevelopedBy": "Xandros", "country": "Canada", "Bit": "32", "Date": "11/15/1999", "DatebyMS": "942645600000"}',
+  '{"name": "Dream", "DevelopedBy": "Unix-like", "country": "Canada", "Bit": "64", "Date": "10/21/2010", "DatebyMS": "1287637200000"}',
+  '{"name": "Handy", "DevelopedBy": "Arpinux", "country": "France", "Bit": "64", "Date": "09/15/2013", "DatebyMS": "1379221200000"}',
+  '{"name": "Symphony", "DevelopedBy": "Ryan P Quinn", "country": "Unknown", "Bit": "64", "Date": "06/29/2011", "DatebyMS": "1309323600000"}',
+  '{"name": "Skole", "DevelopedBy": "Skolelinux team", "country": "Norway", "Bit": "64", "Date": "06/20/2004", "DatebyMS": "1087707600000"}',
+  '{"name": "Maemo", "DevelopedBy": "Hildon Foundation", "country": "Unknown", "Bit": "64", "Date": "11/01/2011", "DatebyMS": "1320123600000"}',  '{"name": "Kali", "DevelopedBy": "Offensive Security", "country": "Unknown", "Bit": "64", "Date": "02/09/2015", "DatebyMS": "1423461600000"}
+].join(",\n  ") + "\n]";
 
 if (typeof module != "undefined" && module.exports)
   module.exports = LINUX;
