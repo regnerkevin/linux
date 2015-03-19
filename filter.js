8573a664c3a49444d5c844dafdf033b860588638
Kevin Regner <krw10108@saintjoe.edu>
2015-03-19 02:37:16 -0500

    Working on a filter for the code.

--- filter.js
+++ filter.js
@@ -1,1 +1,16 @@
-sdlkgjasdljkgbaljksdfbgojksdgowdjfb 
+var data = require('./linux.js');
+var GameList = JSON.parse(data);
+function filter(array, test) {
+	var passed [];
+	for (var i = 0; i < array.length; i++) {
+		if (test(array[i]);
+			passed.push(array[i]);
+	}
+
+	return passed;
+
+}
+
+print(JSON.stringify(filter(LINUX, function(linux) {
+	return linux.YearReleased > 2008;
+})));
